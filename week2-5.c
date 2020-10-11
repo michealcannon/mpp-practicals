215b64174f0b08a2ec6f260878ff232d9015652c
micheal cannon <cannonmicheal@hotmail.com>
2020-10-11 12:48:35 +0100

    added week2-5.c

---
+++ week2-5.c
@@ -0,0 +1,24 @@
+/* ask user for number n, prints sum of 1, 2..n*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    int sum = 0;
+    printf("Please enter a number");
+    scanf("%d", &n);  /* gets number input, specifies format, assigns to n*/
+
+    for (int i = 1; i<=n; i++)
+    {
+        if (((i%3)==0)||((i%5)==0))
+        {
+            sum +=i;
+        }
+    }
+
+    printf("The sum of 1 to %d was %d\n", n, sum);
+
+    return 0;
+}
